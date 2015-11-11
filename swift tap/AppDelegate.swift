9a2d2967e503ebf98501771f7ee70ccb73a7ba92
Gordon Childs <gordon.childs@gmail.com>
2015-11-11 13:35:06 +1100

    stuff

--- swift tap/AppDelegate.swift
+++ swift tap/AppDelegate.swift
@@ -47,8 +47,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		}
 
 		let tapProcess: @convention(c) (MTAudioProcessingTap, CMItemCount, MTAudioProcessingTapFlags, UnsafeMutablePointer<AudioBufferList>, UnsafeMutablePointer<CMItemCount>, UnsafeMutablePointer<MTAudioProcessingTapFlags>) -> Void = {
-			(tap, itemCount, flags, bufferListPtr, itemCountPtr, flagsPtr) -> Void in
-			print("callback \(tap, itemCount, flags, bufferListPtr, itemCountPtr, flagsPtr)\n")
+			(tap, numberFrames, flags, bufferListInOut, numberFramesOut, flagsOut) -> Void in
+			print("callback \(tap, numberFrames, flags, bufferListInOut, numberFramesOut, flagsOut)\n")
+
+			
+//			 status = MTAudioProcessingTapGetSourceAudio(tap, numberFrames, bufferListInOut, flagsOut, NULL, numberFramesOut);
 		}
 		
 		var callbacks = MTAudioProcessingTapCallbacks(
@@ -61,7 +64,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 			process: tapProcess)
 		
 		var tap: Unmanaged<MTAudioProcessingTap>?
-		let err = MTAudioProcessingTapCreate(kCFAllocatorDefault, &callbacks, kMTAudioProcessingTapCreationFlag_PreEffects, &tap)
+		let err = MTAudioProcessingTapCreate(kCFAllocatorDefault, &callbacks, kMTAudioProcessingTapCreationFlag_PostEffects, &tap)
 		
 		print("err: \(err)\n")
 		if err == noErr {
@@ -87,29 +90,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		doit()
 		return true
 	}
-
-	func applicationWillResignActive(application: UIApplication) {
-		// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-		// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-	}
-
-	func applicationDidEnterBackground(application: UIApplication) {
-		// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-		// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-	}
-
-	func applicationWillEnterForeground(application: UIApplication) {
-		// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-	}
-
-	func applicationDidBecomeActive(application: UIApplication) {
-		// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-	}
-
-	func applicationWillTerminate(application: UIApplication) {
-		// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-	}
-
 
 }
 
