1f6b62b283c36221df637fba0aee9aa38027578b
Gordon Childs <gordon.childs@gmail.com>
2018-11-29 22:46:13 +0100

    updated to swift 4.2, fix tap leak, properly pass clientInfo to tap, use KVO to observe asset and player readiness. try and fail to make all remote assets work. update readme.

--- Readme.md
+++ Readme.md
@@ -1,3 +1,12 @@
-An example of how to get going with `MTAudioProcessingTap` audio “taps” in Swift 3. The tap itself is not particularly difficult, but the c-callbacks and casts in Swift can take many hours of googling and trawling [stackoverflow](https://stackoverflow.com) (at least for me).
+An minimal example of an`MTAudioProcessingTap` audio “tap” in Swift 4.2. The tap itself is not particularly difficult, but the c-callbacks and casts in Swift can take many hours of googling and trawling [stackoverflow](https://stackoverflow.com) (at least for me).
 
 Includes attaching an `MTAudioProcessingTap` to a remote `AVPlayer`, c callbacks in swift, casting self to and from `UnsafeMutableRawPointer`, `MTAudioProcessingTapCreate`, `AVMutableAudioMixInputParameters `.
+
+Limitations: the tap does not seem to work with most remote audio assets. There are zero tracks. It does seem to work with a remote file (maybe content-length is important?).
+I've tried waiting for tracks and ready status on the `AVPlayerItem`, but the thing that seems to matter most is the underlying `AVAsset`'s tracks.
+
+**N.B** It's not clear that it's even a good idea to implement your audio tap in Swift. The documentation for `MTAudioProcessingTapProcessCallback` says:
+
+> A processing tap is a real-time operation, so the general Core Audio limitations for real-time processing apply.  For example, care should be taken not to allocate memory or call into blocking system calls, as this will interfere with the real-time nature of audio playback.
+
+I'm pretty sure that means using swift could cause you clicks and pops. Besides, Swift makes getting at the samples fairly tedious. [More info here](http://atastypixel.com/blog/four-common-mistakes-in-audio-development/). 
--- swift tap/AppDelegate.swift
+++ swift tap/AppDelegate.swift
@@ -12,34 +12,17 @@ import MediaToolbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+	
 	var window: UIWindow?
-
+	
 	var player: AVPlayer?
-
+	var playerItem: AVPlayerItem! = nil
+	
 	let tapInit: MTAudioProcessingTapInitCallback = {
 		(tap, clientInfo, tapStorageOut) in
 		
-		let nonOptionalSelf = clientInfo!.assumingMemoryBound(to: AppDelegate.self).pointee
-		
-		print("init \(tap, clientInfo, tapStorageOut, nonOptionalSelf)\n")
-		//			tapStorageOut.assignFrom(source:clientInfo, count: 1)
-		//			tapStorageOut.init(clientInfo)
-	}
-	
-	let tapFinalize: MTAudioProcessingTapFinalizeCallback = {
-		(tap) in
-		print("finalize \(tap)\n")
-	}
-	
-	let tapPrepare: MTAudioProcessingTapPrepareCallback = {
-		(tap, b, c) in
-		print("prepare: \(tap, b, c)\n")
-	}
-	
-	let tapUnprepare: MTAudioProcessingTapUnprepareCallback = {
-		(tap) in
-		print("unprepare \(tap)\n")
+		// Make tap storage the same as clientInfo. I guess you might want them to be different.
+		tapStorageOut.pointee = clientInfo
 	}
 	
 	let tapProcess: MTAudioProcessingTapProcessCallback = {
@@ -48,49 +31,75 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 		
 		let status = MTAudioProcessingTapGetSourceAudio(tap, numberFrames, bufferListInOut, flagsOut, nil, numberFramesOut)
 		print("get audio: \(status)\n")
-	}
-
-	func doit() {
-		let url = URL(string: "http://live-radio01.mediahubaustralia.com/2LRW/mp3/")!
-		let playerItem = AVPlayerItem(url: url)
 		
+		let pointer = Unmanaged<AppDelegate>.fromOpaque(MTAudioProcessingTapGetStorage(tap))
+		let vc = pointer.takeUnretainedValue()
+		print("viewController \(vc)")
+	}
+	
+	let tapFinalize: MTAudioProcessingTapFinalizeCallback = {
+		(tap) in
+		print("finalize \(tap)\n")
+	}
+	
+	var tracksObserver: NSKeyValueObservation? = nil
+	var statusObservation: NSKeyValueObservation? = nil
+	
+	// assumes tracks are loaded
+	func installTap(playerItem: AVPlayerItem) {
 		var callbacks = MTAudioProcessingTapCallbacks(
 			version: kMTAudioProcessingTapCallbacksVersion_0,
 			clientInfo: UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque()),
 			init: tapInit,
 			finalize: tapFinalize,
-			prepare: tapPrepare,
-			unprepare: tapUnprepare,
+			prepare: nil,
+			unprepare: nil,
 			process: tapProcess)
 		
 		var tap: Unmanaged<MTAudioProcessingTap>?
 		let err = MTAudioProcessingTapCreate(kCFAllocatorDefault, &callbacks, kMTAudioProcessingTapCreationFlag_PostEffects, &tap)
+		assert(noErr == err);
 		
-		print("err: \(err)\n")
-		if err == noErr {
-		}
-
-		print("tracks? \(playerItem.asset.tracks)\n")
-		
-		let audioTrack = playerItem.asset.tracks(withMediaType: AVMediaTypeAudio).first!
+		// let audioTrack = playerItem.tracks.first!.assetTrack!
+		let audioTrack = playerItem.asset.tracks(withMediaType: AVMediaType.audio).first!
 		let inputParams = AVMutableAudioMixInputParameters(track: audioTrack)
-		inputParams.audioTapProcessor = tap?.takeUnretainedValue()
+		inputParams.audioTapProcessor = tap?.takeRetainedValue()
 		
-		// print("inputParms: \(inputParams), \(inputParams.audioTapProcessor)\n")
 		let audioMix = AVMutableAudioMix()
 		audioMix.inputParameters = [inputParams]
 		
 		playerItem.audioMix = audioMix
-		
-		player = AVPlayer(playerItem: playerItem)
-		player?.play()
 	}
-
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-		// Override point for customization after application launch.
+	
+	func doit() {
+		// some remote resources work. maybe those with ContentLength?
+		 let s = "https://bsrr00.s3.amazonaws.com/98iN.mp3"  // has tracks
+//		let s = "http://live-radio01.mediahubaustralia.com/2LRW/mp3/"	// doesn't work any more
+		let url = URL(string: s)!
+//		 let url = Bundle.main.url(forResource: "foo", withExtension: "m4a")!	 // local resource works
+		
+		self.playerItem = AVPlayerItem(url: url)
+		self.player = AVPlayer(playerItem: self.playerItem)
+		
+		self.tracksObserver = playerItem.observe(\AVPlayerItem.tracks) {
+			[unowned self] item, change in
+			NSLog("tracks change \(item.tracks)")
+			NSLog("asset tracks (btw) \(item.asset.tracks)")
+			self.installTap(playerItem: self.playerItem)
+		}
+		
+		self.statusObservation = playerItem.observe(\AVPlayerItem.status) {
+			[unowned self] object, change in
+			NSLog("playerItem status change \(object.status.rawValue)")
+			if object.status == .readyToPlay {
+				self.player?.play()
+			}
+		}
+	}
+	
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		doit()
 		return true
 	}
-
 }
 
